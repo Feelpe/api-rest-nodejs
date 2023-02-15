ff1ae2bb8440be650218a1574bbb1a40b71851d1
felipe <felipesilvasantoslawliet1@gmail.com>
2023-02-15 16:43:52 -0300

    feat: creating tables with knex

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,19 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions').select('*')
 
-  return tables
+  return transactions
+
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+
+  // return transaction
 })
 
 app
