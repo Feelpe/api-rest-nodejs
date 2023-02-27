e36101a65535535aed39943737bd690eb04c1843
felipe <felipesilvasantoslawliet1@gmail.com>
2023-02-27 16:18:58 -0300

    feat: transaction route

--- db/migrations/20230215190812_add-session-id-to-transactions.ts
+++ db/migrations/20230215190812_add-session-id-to-transactions.ts
@@ -8,6 +8,6 @@ export async function up(knex: Knex): Promise<void> {
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transactions', (table) => {
-    table.dropColumn('session')
+    table.dropColumn('session_id')
   })
 }
